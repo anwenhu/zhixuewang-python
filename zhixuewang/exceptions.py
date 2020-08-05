61b7802256522d689d09992f8cf3bb85d168df39
anwenhu <anemailpocket@163.com>
2020-08-05 22:55:38 +0800

    add some __repr__ methods and so on.

--- zhixuewang/exceptions.py
+++ zhixuewang/exceptions.py
@@ -24,10 +24,17 @@ class UserDefunctError(LoginError):
     def __init__(self, value=None):
         super().__init__(value or "用户已失效!")
 
+class RoleError(Error):
+    def __init__(self, value=None):
+        self.value = value or "账号是未知用户"
+
+    def __str__(self):
+        return str(self.value)
+
 
 class ArgError(Error):
     def __init__(self, value=None):
-        super().__init__(value or "请输入正确的参数!")
+        self.value = value or "请输入正确的参数!"
 
     def __str__(self):
         return str(self.value)
