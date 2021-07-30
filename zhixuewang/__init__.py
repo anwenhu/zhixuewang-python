c2a83670281f1ec6c0dd3d578081774afcded924
anwenhu <anemailpocket@163.com>
2021-07-30 21:45:09 +0800

    update to 1.0.6

--- zhixuewang/__init__.py
+++ zhixuewang/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "anwenhu"
-__date__ = "2021/7/28 18:00"
-__version__ = "1.0.5"
+__date__ = "2021/7/30 21:30"
+__version__ = "1.0.6"
 
 from zhixuewang.zxw import (login, login_id, rewrite_str, login_student, login_teacher,
                             login_student_id, login_teacher_id, get_session, get_session_id)
