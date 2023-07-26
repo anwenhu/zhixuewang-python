02e04d201f6d09095a311d185397c4cea6813c4b
anwenhu <anemailpocket@163.com>
2023-07-26 12:29:21 +0800

    升级到1.1.9

--- zhixuewang/__init__.py
+++ zhixuewang/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "anwenhu"
-__date__ = "2023/7/7 7:7"
-__version__ = "1.1.8"
+__date__ = "2023/7/26 12:30"
+__version__ = "1.1.9"
 
 from zhixuewang.account import (login, login_id, rewrite_str, login_student, login_teacher,
                                 login_student_id, login_teacher_id, load_account)
